f84a9c3cc6b7116a00cdd9bef38c3c6ecda8e10d
Ryan VanderVeen <92502563+ryanvanderveen@users.noreply.github.com>
2023-09-20 15:08:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const client = new Client({
 });
 
 client.on('ready', () => {
-  console.log('The bot is online!');
+  console.log('The bot is online!!');
 });
 
 const configuration = new Configuration({
